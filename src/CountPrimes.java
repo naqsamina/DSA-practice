8c5d2694c3dfb8f2d570d9dca8f6698afcc77a2a
Samina Naqvi <naqsamina786@gmail.com>
2026-09-03 13:26:19 +0530

    CountPrimes

--- src/CountPrimes.java
+++ src/CountPrimes.java
@@ -1,4 +1,27 @@
 package src;
 
 public class CountPrimes {
-}
+        public static int solve(int A) {
+            int count =0;
+            for(int i =2;i<=A;i++){
+                int factors = 0;
+                for(int j =1;j<=i;j++){
+                    if(i%j==0){
+                        factors++;
+                    }
+
+                }
+                if(factors==2){
+                    count++;
+                }
+            }
+            return count;
+        }
+    public static void main(String[] args) {
+        System.out.println(solve(10));
+
+    }
+    }
+
+
+
